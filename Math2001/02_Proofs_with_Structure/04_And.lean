d7abfa0c14c9731f06b2a6a2702db245e8649f84
Hanson Char <hanson.char@gmail.com>
2025-07-01 14:40:50 -0700

    2.4.2. Example

--- Math2001/02_Proofs_with_Structure/04_And.lean
+++ Math2001/02_Proofs_with_Structure/04_And.lean
@@ -8,6 +8,8 @@ math2001_init
   ## 2.4. “And”
 
   ### 2.4.1. Example
+
+  Remark: uses obtain to split a conjunction into two hypothesis.
 -/
 
 example {x y : ℤ} (h : 2 * x - y = 4 ∧ y - x + 1 = 2) : x = 5 := by
@@ -17,15 +19,45 @@ example {x y : ℤ} (h : 2 * x - y = 4 ∧ y - x + 1 = 2) : x = 5 := by
     _ = 4 + 2 - 1 := by rw [h1, h2]
     _ = 5 := by ring
 
+/-!
+  ### 2.4.2. Example
 
+  Interesting parts:
+  1. introduce a new hypothesis
+  1. prove via the application (via `apply`) of `abs_le_of_sq_le_sq'`
+  1. use of `.` in proving a newly introduced hypothesis
+  1. the use of `numbers` and nothing else to prove a goal.
+-/
 example {p : ℚ} (hp : p ^ 2 ≤ 8) : p ≥ -5 := by
-  have hp' : -3 ≤ p ∧ p ≤ 3
-  · apply abs_le_of_sq_le_sq'
-    calc
-      p ^ 2 ≤ 9 := by addarith [hp]
-      _ = 3 ^ 2 := by numbers
-    numbers
-  sorry
+  have hp' : -3 ≤ p ∧ p ≤ 3 -- conjure up `hp'` but we need to prove it.
+  · apply abs_le_of_sq_le_sq' -- match `-3 ≤ p ∧ p ≤ 3` and turn into 2 subgoals.
+    calc  -- First subgoal: `p ^ 2 ≤ 3 ^ 2`.
+        p ^ 2 ≤ 9 := by addarith [hp]
+        _ = 3 ^ 2 := by numbers
+    numbers -- Second subgoal: `0 ≤ 3`.
+  -- At this point, `hp'` has been established.
+  obtain ⟨h1, h2⟩ := hp' -- Split `hp'` into:
+  -- h1: -3 ≤ p
+  -- h2: p ≤ 3
+  calc
+    p ≥ -3 := by rel [h1]
+    _ ≥ -5 := by numbers
+
+-- Same, but perhaps a bit easier to read.
+example {p : ℚ} (hp : p ^ 2 ≤ 8) : p ≥ -5 := by
+  have hp' : -3 ≤ p ∧ p ≤ 3 -- conjure up `hp'` but we need to prove it.
+  · apply abs_le_of_sq_le_sq' -- match `-3 ≤ p ∧ p ≤ 3` and turn into 2 subgoals.
+    . calc  -- First subgoal: `p ^ 2 ≤ 3 ^ 2`.
+        p ^ 2 ≤ 9 := by addarith [hp]
+        _ = 3 ^ 2 := by numbers
+    . numbers -- Second subgoal: `0 ≤ 3`.
+  -- At this point, `hp'` has been established.
+  obtain ⟨h1, h2⟩ := hp' -- Split `hp'` into:
+  -- h1: -3 ≤ p
+  -- h2: p ≤ 3
+  calc
+    p ≥ -3 := by rel [h1]
+    _ ≥ -5 := by numbers
 
 example {a b : ℝ} (h1 : a - 5 * b = 4) (h2 : b + 2 = 3) : a = 9 ∧ b = 1 := by
   constructor
