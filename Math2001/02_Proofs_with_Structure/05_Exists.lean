a2b588c4f404973969da6b368088ca3b1caba824
Hanson Char <hanson.char@gmail.com>
2025-07-03 11:53:51 -0700

    2.5.2. Example

--- Math2001/02_Proofs_with_Structure/05_Exists.lean
+++ Math2001/02_Proofs_with_Structure/05_Exists.lean
@@ -17,17 +17,33 @@ example {a : ℚ} (h : ∃ b : ℚ, a = b ^ 2 + 1) : a > 0 := by
     a = b ^ 2 + 1 := hb
     _ > 0 := by extra
 
-
+/-!
+  ### 2.5.2. Example
+-/
 example {t : ℝ} (h : ∃ a : ℝ, a * t < 0) : t ≠ 0 := by
   obtain ⟨x, hxt⟩ := h
-  have H := le_or_gt x 0
+  have H := le_or_gt x 0 -- Note this hypothesis is always true, i.e., needs no justification
   obtain hx | hx := H
-  · have hxt' : 0 < (-x) * t := by addarith [hxt]
+  · have hxt' : 0 < (-x) * t := by addarith [hxt] -- hx: x ≤ 0
     have hx' : 0 ≤ -x := by addarith [hx]
-    cancel -x at hxt'
-    apply ne_of_gt
+    -- We can cancel `-x` from `0 < -x * t` even if we only have `0 ≤ -x`.
+    -- `cancel` appears to be smart enought to figure that `x` could not be zero in this case.
+    cancel -x at hxt' -- interesting use of `cancel`, directly modifying `hxt'`!
+    apply ne_of_gt  -- ⊢ 0 < t
     apply hxt'
-  · sorry
+  . have hxt' : 0 < -x * t := by addarith [hxt] -- hx: x > 0
+    have ht : 0 < x * (-t) := by
+      calc
+        0 < -x * t := hxt'
+        _ = x * -t := by ring
+    cancel x at ht  -- we can cancel `x` from `0 < x * -t` when we can establish that `x > 0`.
+    have ht' : t < 0 := by addarith [ht]
+    apply ne_of_lt  -- ⊢ t < 0
+    exact ht'
+
+-- It appears that for `cancel` to work we have to move all the terms to the positive side.
+example {t x : ℝ} (h1 : 0 < x * t) (h2 : x > 0) : t > 0 := by
+  cancel x at h1
 
 example : ∃ n : ℤ, 12 * n = 84 := by
   use 7
@@ -87,3 +103,4 @@ example {n : ℤ} : ∃ a, 2 * a ^ 3 ≥ n * a + 7 := by
 example {a b c : ℝ} (ha : a ≤ b + c) (hb : b ≤ a + c) (hc : c ≤ a + b) :
     ∃ x y z, x ≥ 0 ∧ y ≥ 0 ∧ z ≥ 0 ∧ a = y + z ∧ b = x + z ∧ c = x + y := by
   sorry
+–
