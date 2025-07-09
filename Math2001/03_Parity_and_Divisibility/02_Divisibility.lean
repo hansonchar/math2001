20044f9033bde1fd70e7ac0df430e0c8f59e90ad
Hanson Char <hanson.char@gmail.com>
2025-07-09 16:31:12 -0700

    Section 3.2

--- Math2001/03_Parity_and_Divisibility/02_Divisibility.lean
+++ Math2001/03_Parity_and_Divisibility/02_Divisibility.lean
@@ -37,9 +37,15 @@ example {a b : ℤ} (hab : a ∣ b) : a ∣ b ^ 2 + 2 * b := by
 example {a b c : ℕ} (hab : a ∣ b) (hbc : b ^ 2 ∣ c) : a ^ 2 ∣ c := by
   sorry
 
+/-!
+  ### 3.2.5. Example
+-/
 example {x y z : ℕ} (h : x * y ∣ z) : x ∣ z := by
   sorry
 
+/-!
+  ### 3.2.6. Example
+-/
 example : ¬(5 : ℤ) ∣ 12 := by
   apply Int.not_dvd_of_exists_lt_and_lt
   use 2
@@ -47,7 +53,9 @@ example : ¬(5 : ℤ) ∣ 12 := by
   · numbers -- show `5 * 2 < 12`
   · numbers -- show `12 < 5 * (2 + 1)`
 
-
+/-!
+  ### 3.2.7. Example
+-/
 example {a b : ℕ} (hb : 0 < b) (hab : a ∣ b) : a ≤ b := by
   obtain ⟨k, hk⟩ := hab
   have H1 :=
@@ -61,36 +69,46 @@ example {a b : ℕ} (hb : 0 < b) (hab : a ∣ b) : a ≤ b := by
     _ ≤ a * k := by rel [H]
     _ = b := by rw [hk]
 
-
+/-!
+  ### 3.2.8. Example
+-/
 example {a b : ℕ} (hab : a ∣ b) (hb : 0 < b) : 0 < a := by
   sorry
 
-/-! # Exercises -/
+/-! ### 3.2.9 Exercises -/
 
-
+-- Exercise 3.2.9.1
 example (t : ℤ) : t ∣ 0 := by
   sorry
 
+-- Exercise 3.2.9.2
 example : ¬(3 : ℤ) ∣ -10 := by
   sorry
 
+-- Exercise 3.2.9.3
 example {x y : ℤ} (h : x ∣ y) : x ∣ 3 * y - 4 * y ^ 2 := by
   sorry
 
+-- Exercise 3.2.9.4
 example {m n : ℤ} (h : m ∣ n) : m ∣ 2 * n ^ 3 + n := by
   sorry
 
+-- Exercise 3.2.9.5
 example {a b : ℤ} (hab : a ∣ b) : a ∣ 2 * b ^ 3 - b ^ 2 + 3 * b := by
   sorry
 
+-- Exercise 3.2.9.6
 example {k l m : ℤ} (h1 : k ∣ l) (h2 : l ^ 3 ∣ m) : k ^ 3 ∣ m := by
   sorry
 
+-- Exercise 3.2.9.7
 example {p q r : ℤ} (hpq : p ^ 3 ∣ q) (hqr : q ^ 2 ∣ r) : p ^ 6 ∣ r := by
   sorry
 
+-- Exercise 3.2.9.8
 example : ∃ n : ℕ, 0 < n ∧ 9 ∣ 2 ^ n - 1 := by
   sorry
 
+-- Exercise 3.2.9.9
 example : ∃ a b : ℤ, 0 < b ∧ b < a ∧ a - b ∣ a + b := by
   sorry
