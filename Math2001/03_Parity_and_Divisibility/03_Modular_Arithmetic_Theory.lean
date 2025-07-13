690529132e1528d166659e3ca05cdf15c9467ff1
Hanson Char <hanson.char@gmail.com>
2025-07-12 22:51:18 -0700

    3.3. Modular arithmetic: theory

--- Math2001/03_Parity_and_Divisibility/03_Modular_Arithmetic_Theory.lean
+++ Math2001/03_Parity_and_Divisibility/03_Modular_Arithmetic_Theory.lean
@@ -4,14 +4,26 @@ import Library.Theory.ModEq.Defs
 
 math2001_init
 
+/-!
+  ## 3.3. Modular arithmetic: theory
+-/
 
+/-!
+  ### 3.3.1. Example
+-/
 example : 11 ≡ 3 [ZMOD 4] := by
   use 2
   numbers
 
+/-!
+  ### 3.3.2. Example
+-/
 example : -5 ≡ 1 [ZMOD 3] := by
   sorry
 
+/-!
+  ### 3.3.3. Example
+-/
 theorem Int.ModEq.add {n a b c d : ℤ} (h1 : a ≡ b [ZMOD n]) (h2 : c ≡ d [ZMOD n]) :
     a + c ≡ b + d [ZMOD n] := by
   dsimp [Int.ModEq] at *
@@ -23,14 +35,22 @@ theorem Int.ModEq.add {n a b c d : ℤ} (h1 : a ≡ b [ZMOD n]) (h2 : c ≡ d [Z
     _ = n * x + n * y := by rw [hx, hy]
     _ = n * (x + y) := by ring
 
-
+/-!
+  ### 3.3.4. Exercise
+-/
 theorem Int.ModEq.sub {n a b c d : ℤ} (h1 : a ≡ b [ZMOD n]) (h2 : c ≡ d [ZMOD n]) :
     a - c ≡ b - d [ZMOD n] := by
   sorry
 
+/-!
+  ### 3.3.5. Exercise
+-/
 theorem Int.ModEq.neg {n a b : ℤ} (h1 : a ≡ b [ZMOD n]) : -a ≡ -b [ZMOD n] := by
   sorry
 
+/-!
+  ### 3.3.6. Example
+-/
 theorem Int.ModEq.mul {n a b c d : ℤ} (h1 : a ≡ b [ZMOD n]) (h2 : c ≡ d [ZMOD n]) :
     a * c ≡ b * d [ZMOD n] := by
   obtain ⟨x, hx⟩ := h1
@@ -41,7 +61,9 @@ theorem Int.ModEq.mul {n a b c d : ℤ} (h1 : a ≡ b [ZMOD n]) (h2 : c ≡ d [Z
     _ = n * x * c + b * (n * y) := by rw [hx, hy]
     _ = n * (x * c + b * y) := by ring
 
-
+/-!
+  ### 3.3.8. Example
+-/
 theorem Int.ModEq.pow_two (h : a ≡ b [ZMOD n]) : a ^ 2 ≡ b ^ 2 [ZMOD n] := by
   obtain ⟨x, hx⟩ := h
   use x * (a + b)
@@ -50,19 +72,33 @@ theorem Int.ModEq.pow_two (h : a ≡ b [ZMOD n]) : a ^ 2 ≡ b ^ 2 [ZMOD n] := b
     _ = n * x * (a + b) := by rw [hx]
     _ = n * (x * (a + b)) := by ring
 
+/-!
+  ### 3.3.9. Exercise
 
+  Cubing rule for modular arithmetic.
+-/
 theorem Int.ModEq.pow_three (h : a ≡ b [ZMOD n]) : a ^ 3 ≡ b ^ 3 [ZMOD n] := by
   sorry
 
+/-!
+  Power fule for modular arithmetic.
+-/
 theorem Int.ModEq.pow (k : ℕ) (h : a ≡ b [ZMOD n]) : a ^ k ≡ b ^ k [ZMOD n] :=
   sorry -- we'll prove this later in the book
 
+/-!
+  ### 3.3.10. Example
 
+  Reflexivity rule for modular arithmetic.
+-/
 theorem Int.ModEq.refl (a : ℤ) : a ≡ a [ZMOD n] := by
   use 0
   ring
 
 
+/-!
+  ### 3.3.11. Example
+-/
 example {a b : ℤ} (ha : a ≡ 2 [ZMOD 4]) :
     a * b ^ 2 + a ^ 2 * b + 3 * a ≡ 2 * b ^ 2 + 2 ^ 2 * b + 3 * 2 [ZMOD 4] := by
   obtain ⟨x, hx⟩ := ha
@@ -90,29 +126,36 @@ example {a b : ℤ} (ha : a ≡ 2 [ZMOD 4]) :
   apply Int.ModEq.refl
   apply ha
 
-/-! # Exercises -/
+/-! 3.3.12. Exercises -/
 
 
+-- Exercise 3.3.12.1
 example : 34 ≡ 104 [ZMOD 5] := by
   sorry
 
+-- Exercise 3.3.12.2
 theorem Int.ModEq.symm (h : a ≡ b [ZMOD n]) : b ≡ a [ZMOD n] := by
   sorry
 
+-- Exercise 3.3.12.3
 theorem Int.ModEq.trans (h1 : a ≡ b [ZMOD n]) (h2 : b ≡ c [ZMOD n]) :
     a ≡ c [ZMOD n] := by
   sorry
 
+-- Exercise 3.3.12.4
 example : a + n * c ≡ a [ZMOD n] := by
   sorry
 
 
+-- Exercise 3.3.12.6
 example {a b : ℤ} (h : a ≡ b [ZMOD 5]) : 2 * a + 3 ≡ 2 * b + 3 [ZMOD 5] := by
   sorry
 
+-- Exercise 3.3.12.7
 example {m n : ℤ} (h : m ≡ n [ZMOD 4]) : 3 * m - 1 ≡ 3 * n - 1 [ZMOD 4] := by
   sorry
 
+-- Exercise 3.3.12.8
 example {k : ℤ} (hb : k ≡ 3 [ZMOD 5]) :
     4 * k + k ^ 3 + 3 ≡ 4 * 3 + 3 ^ 3 + 3 [ZMOD 5] := by
   sorry
