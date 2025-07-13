36372e8eeb877f9c8948401dbdf3ffb40119ecbf
Hanson Char <hanson.char@gmail.com>
2025-07-13 13:38:05 -0700

    Example 3.4.3-4.

--- Math2001/03_Parity_and_Divisibility/04_Modular_Arithmetic_Calculations.lean
+++ Math2001/03_Parity_and_Divisibility/04_Modular_Arithmetic_Calculations.lean
@@ -40,23 +40,29 @@ example : ∃ a : ℤ, 6 * a ≡ 4 [ZMOD 11] := by
   use 8
   calc
     (6:ℤ) * 8 = 4 + 4 * 11 := by numbers
-    _ ≡ 4 [ZMOD 11] := by extra
+    _ ≡ 4 [ZMOD 11] := by extra -- Another example of using `extra`.
 
 /-!
   ### 3.4.4. Example
+
+  Interesting example of using `mod_cases`.
 -/
 example {x : ℤ} : x ^ 3 ≡ x [ZMOD 3] := by
   mod_cases hx : x % 3
+  -- hx : x ≡ 0 [ZMOD 3]
   calc
     x ^ 3 ≡ 0 ^ 3 [ZMOD 3] := by rel [hx]
     _ = 0 := by numbers
     _ ≡ x [ZMOD 3] := by rel [hx]
+  -- hx : x ≡ 1 [ZMOD 3]
   calc
     x ^ 3 ≡ 1 ^ 3 [ZMOD 3] := by rel [hx]
     _ = 1 := by numbers
     _ ≡ x [ZMOD 3] := by rel [hx]
+  -- hx : x ≡ 2 [ZMOD 3]
   calc
     x ^ 3 ≡ 2 ^ 3 [ZMOD 3] := by rel [hx]
+    -- Notice this repeated pattern of re-expressing in a specific form of addition.
     _ = 2 + 3 * 2 := by numbers
     _ ≡ 2 [ZMOD 3] := by extra
     _ ≡ x [ZMOD 3] := by rel [hx]
