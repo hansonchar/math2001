757690238acf90faef71ef53c1ed81980e1dc106
Hanson Char <hanson.char@gmail.com>
2025-07-13 12:02:49 -0700

    3.4. Modular arithmetic: calculations

--- Math2001/03_Parity_and_Divisibility/04_Modular_Arithmetic_Calculations.lean
+++ Math2001/03_Parity_and_Divisibility/04_Modular_Arithmetic_Calculations.lean
@@ -4,12 +4,20 @@ import Library.Tactic.ModEq
 
 math2001_init
 
+/-!
+  ## 3.4. Modular arithmetic: calculations
+-/
 
+/-!
+  ### 3.4.1. Example
+-/
 example {a b : ℤ} (ha : a ≡ 2 [ZMOD 4]) :
     a * b ^ 2 + a ^ 2 * b + 3 * a ≡ 2 * b ^ 2 + 2 ^ 2 * b + 3 * 2 [ZMOD 4] := by
   rel [ha]
 
-
+/-!
+  ### 3.4.2. Example
+-/
 example {a b : ℤ} (ha : a ≡ 4 [ZMOD 5]) (hb : b ≡ 3 [ZMOD 5]) :
     a * b + b ^ 3 + 3 ≡ 2 [ZMOD 5] :=
   calc
@@ -18,14 +26,18 @@ example {a b : ℤ} (ha : a ≡ 4 [ZMOD 5]) (hb : b ≡ 3 [ZMOD 5]) :
     _ = 2 + 5 * 8 := by numbers
     _ ≡ 2 [ZMOD 5] := by extra
 
-
+/-!
+  ### 3.4.3. Example
+-/
 example : ∃ a : ℤ, 6 * a ≡ 4 [ZMOD 11] := by
   use 8
   calc
     (6:ℤ) * 8 = 4 + 4 * 11 := by numbers
     _ ≡ 4 [ZMOD 11] := by extra
 
-
+/-!
+  ### 3.4.4. Example
+-/
 example {x : ℤ} : x ^ 3 ≡ x [ZMOD 3] := by
   mod_cases hx : x % 3
   calc
@@ -42,27 +54,33 @@ example {x : ℤ} : x ^ 3 ≡ x [ZMOD 3] := by
     _ ≡ 2 [ZMOD 3] := by extra
     _ ≡ x [ZMOD 3] := by rel [hx]
 
-/-! # Exercises -/
+/-! ### 3.4.5. Exercises -/
 
-
+-- Exercise 3.4.5.1
 example {n : ℤ} (hn : n ≡ 1 [ZMOD 3]) : n ^ 3 + 7 * n ≡ 2 [ZMOD 3] :=
   sorry
 
+-- Exercise 3.4.5.2
 example {a : ℤ} (ha : a ≡ 3 [ZMOD 4]) :
     a ^ 3 + 4 * a ^ 2 + 2 ≡ 1 [ZMOD 4] :=
   sorry
 
+-- Exercise 3.4.5.3
 example (a b : ℤ) : (a + b) ^ 3 ≡ a ^ 3 + b ^ 3 [ZMOD 3] :=
   sorry
 
+-- Exercise 3.4.5.4
 example : ∃ a : ℤ, 4 * a ≡ 1 [ZMOD 7] := by
   sorry
 
+-- Exercise 3.4.5.5
 example : ∃ k : ℤ, 5 * k ≡ 6 [ZMOD 8] := by
   sorry
 
+-- Exercise 3.4.5.6
 example (n : ℤ) : 5 * n ^ 2 + 3 * n + 7 ≡ 1 [ZMOD 2] := by
   sorry
 
+-- Exercise 3.4.5.7
 example {x : ℤ} : x ^ 5 ≡ x [ZMOD 5] := by
   sorry
