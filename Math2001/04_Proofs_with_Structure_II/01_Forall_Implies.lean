2ce1fd39c6fa7bcd9e063fa5a53df123fcdb4471
Hanson Char <hanson.char@gmail.com>
2025-07-13 22:06:10 -0700

    4. Proofs with structure, II

--- Math2001/04_Proofs_with_Structure_II/01_Forall_Implies.lean
+++ Math2001/04_Proofs_with_Structure_II/01_Forall_Implies.lean
@@ -4,13 +4,23 @@ import Library.Basic
 
 math2001_init
 
+/-!
+  # 4. Proofs with structure, II
+-/
 
+/-!
+  ## 4.1. “For all” and implication
+
+  ### 4.1.1. Example
+-/
 example {a : ℝ} (h : ∀ x, a ≤ x ^ 2 - 2 * x) : a ≤ -1 :=
   calc
     a ≤ 1 ^ 2 - 2 * 1 := by apply h
     _ = -1 := by numbers
 
-
+/-!
+  ### 4.1.2. Example
+-/
 example {n : ℕ} (hn : ∀ m, n ∣ m) : n = 1 := by
   have h1 : n ∣ 1 := by apply hn
   have h2 : 0 < 1 := by numbers
@@ -18,10 +28,15 @@ example {n : ℕ} (hn : ∀ m, n ∣ m) : n = 1 := by
   · apply Nat.le_of_dvd h2 h1
   · apply Nat.pos_of_dvd_of_pos h1 h2
 
-
+/-!
+  ### 4.1.3. Example
+-/
 example {a b : ℝ} (h : ∀ x, x ≥ a ∨ x ≤ b) : a ≤ b := by
   sorry
 
+/-!
+  ### 4.1.4. Example
+-/
 example {a b : ℝ} (ha1 : a ^ 2 ≤ 2) (hb1 : b ^ 2 ≤ 2) (ha2 : ∀ y, y ^ 2 ≤ 2 → y ≤ a)
     (hb2 : ∀ y, y ^ 2 ≤ 2 → y ≤ b) :
     a = b := by
@@ -30,6 +45,9 @@ example {a b : ℝ} (ha1 : a ^ 2 ≤ 2) (hb1 : b ^ 2 ≤ 2) (ha2 : ∀ y, y ^ 2 
     apply ha1
   · sorry
 
+/-!
+  ### 4.1.5. Example
+-/
 example : ∃ b : ℝ, ∀ x : ℝ, b ≤ x ^ 2 - 2 * x := by
   use -1
   intro x
@@ -38,9 +56,15 @@ example : ∃ b : ℝ, ∀ x : ℝ, b ≤ x ^ 2 - 2 * x := by
     _ = x ^ 2 - 2 * x := by ring
 
 
+/-!
+  ### 4.1.6. Example
+-/
 example : ∃ c : ℝ, ∀ x y, x ^ 2 + y ^ 2 ≤ 4 → x + y ≥ c := by
   sorry
 
+/-!
+  ### 4.1.7. Example
+-/
 example : forall_sufficiently_large n : ℤ, n ^ 3 ≥ 4 * n ^ 2 + 7 := by
   dsimp
   use 5
@@ -53,7 +77,9 @@ example : forall_sufficiently_large n : ℤ, n ^ 3 ≥ 4 * n ^ 2 + 7 := by
     _ = 4 * n ^ 2 + 7 + 18 := by ring
     _ ≥ 4 * n ^ 2 + 7 := by extra
 
-
+/-!
+  ### 4.1.8. Example
+-/
 example : Prime 2 := by
   constructor
   · numbers -- show `2 ≤ 2`
@@ -67,30 +93,37 @@ example : Prime 2 := by
   · right
     numbers -- show `2 = 2`
 
-
+/-!
+  ### 4.1.9. Example
+-/
 example : ¬ Prime 6 := by
   apply not_prime 2 3
   · numbers -- show `2 ≠ 1`
   · numbers -- show `2 ≠ 6`
   · numbers -- show `6 = 2 * 3`
 
-/-! # Exercises -/
+/-! ### 4.1.10. Exercises -/
 
-
+-- Exercise 4.1.10.1
 example {a : ℚ} (h : ∀ b : ℚ, a ≥ -3 + 4 * b - b ^ 2) : a ≥ 1 :=
   sorry
 
+-- Exercise 4.1.10.2
 example {n : ℤ} (hn : ∀ m, 1 ≤ m → m ≤ 5 → m ∣ n) : 15 ∣ n := by
   sorry
 
+-- Exercise 4.1.10.3
 example : ∃ n : ℕ, ∀ m : ℕ, n ≤ m := by
   sorry
 
+-- Exercise 4.1.10.4
 example : ∃ a : ℝ, ∀ b : ℝ, ∃ c : ℝ, a + b < c := by
   sorry
 
+-- Exercise 4.1.10.5
 example : forall_sufficiently_large x : ℝ, x ^ 3 + 3 * x ≥ 7 * x ^ 2 + 12 := by
   sorry
 
+-- Exercise 4.1.10.6
 example : ¬(Prime 45) := by
   sorry
