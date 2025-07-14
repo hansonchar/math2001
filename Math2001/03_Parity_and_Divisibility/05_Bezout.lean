369a6cd548f1b633d5492f62a2b73bda96ca098b
Hanson Char <hanson.char@gmail.com>
2025-07-13 18:08:38 -0700

    3.5. Bézout’s identity

--- Math2001/03_Parity_and_Divisibility/05_Bezout.lean
+++ Math2001/03_Parity_and_Divisibility/05_Bezout.lean
@@ -3,7 +3,13 @@ import Library.Basic
 
 math2001_init
 
+/-!
+  ## 3.5. Bézout’s identity
+-/
 
+/-!
+  ### 3.5.1. Example
+-/
 example {n : ℤ} (hn : 8 ∣ 5 * n) : 8 ∣ n := by
   obtain ⟨a, ha⟩ := hn
   use -3 * a + 2 * n
@@ -16,9 +22,15 @@ example {n : ℤ} (hn : 8 ∣ 5 * n) : 8 ∣ n := by
 example {n : ℤ} (hn : 8 ∣ 5 * n) : 8 ∣ n := by
   sorry
 
+/-!
+  ### 3.5.2. Example
+-/
 example {n : ℤ} (h1 : 5 ∣ 3 * n) : 5 ∣ n := by
   sorry
 
+/-!
+  ### 3.5.3. Example
+-/
 example {m : ℤ} (h1 : 8 ∣ m) (h2 : 5 ∣ m) : 40 ∣ m := by
   obtain ⟨a, ha⟩ := h1
   obtain ⟨b, hb⟩ := h2
@@ -29,17 +41,20 @@ example {m : ℤ} (h1 : 8 ∣ m) (h2 : 5 ∣ m) : 40 ∣ m := by
     _ = -15 * (8 * a) + 16 * (5 * b) := by rw [hb]
     _ = 40 * (-3 * a + 2 * b) := by ring
 
-/-! # Exercises -/
+/-! ### 3.5.4. Exercises -/
 
-
+-- Exercise 3.5.4.1.
 example {n : ℤ} (hn : 6 ∣ 11 * n) : 6 ∣ n := by
   sorry
 
+-- Exercise 3.5.4.2.
 example {a : ℤ} (ha : 7 ∣ 5 * a) : 7 ∣ a := by
   sorry
 
+-- Exercise 3.5.4.3.
 example {n : ℤ} (h1 : 7 ∣ n) (h2 : 9 ∣ n) : 63 ∣ n := by
   sorry
 
+-- Exercise 3.5.4.4.
 example {n : ℤ} (h1 : 5 ∣ n) (h2 : 13 ∣ n) : 65 ∣ n := by
   sorry
